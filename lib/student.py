8c73bc2a987e0fc551fbdc5ec9e7d4debc740772
brevre <brendamakena09@gmail.com>
2025-03-02 10:37:32 +0300

    completed lab

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,11 @@
-#!/usr/bin/env python
-
-from user import User
+# lib/student.py
+from user import User  # Ensure correct import
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)  # Call User's constructor
+        self.knowledge = []  # Ensure this attribute is initialized as an empty list
+
+    def learn(self, lesson):
+        """Adds a lesson to the student's knowledge."""
+        self.knowledge.append(lesson)
--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,20 @@
-#!/usr/bin/env python
-
-from user import User
-
+# lib/teacher.py
 import random
+from user import User  # Ensure user.py is correctly imported
 
-class Teacher(User):
+class Teacher(User):  # Teacher inherits from User
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)  # Corrected call to User's constructor
+        self.knowledge = [
+            "str is a data type in Python",
+            "programming is hard, but it's worth it",
+            "JavaScript async web request",
+            "Python function call definition",
+            "object-oriented teacher instance",
+            "programming computers hacking learning terminal",
+            "pipenv install pipenv shell",
+            "pytest -x flag to fail fast",
+        ]
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
